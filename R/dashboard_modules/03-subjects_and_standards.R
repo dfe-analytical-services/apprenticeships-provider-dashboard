9f8cc70a61ad6cab4650da3e09ff782312edab5c
COOPER <Alison.Cooper@EDUCATION.GOV.UK>
2024-09-27 11:01:06 +0100

    run styling checks, need to remove commented code

--- R/dashboard_modules/03-subjects_and_standards.R
+++ R/dashboard_modules/03-subjects_and_standards.R
@@ -3,17 +3,13 @@
 sas_parquet <- read_sas("data/apprenticeships_data_0.parquet")
 
 # Create static lists of options for dropdowns
-sas_provider_choices <- data_choices(data = sas_parquet, column = "provider_name")
-# Providers should be in alphabetical order
-sas_provider_choices <- sort(sas_provider_choices)
+sas_provider_choices <- sort(data_choices(data = sas_parquet, column = "provider_name"))
 sas_year_choices <- sort(data_choices(data = sas_parquet, column = "year"),
   decreasing = TRUE
 )
 sas_measure_choices <- data_choices(data = sas_parquet, column = "measure")
-
 sas_level_choices <- data_choices(data = sas_parquet, column = "apps_Level")
-
-sas_standard_choices <- data_choices(data = sas_parquet, column = "std_fwk_name")
+sas_standard_choices <- sort(data_choices(data = sas_parquet, column = "std_fwk_name"))
 
 subjects_standards_ui <- function(id) {
   div(
@@ -23,13 +19,6 @@ subjects_standards_ui <- function(id) {
       style = "min-height: 100%; height: 100%; overflow-y: visible;",
       bslib::layout_column_wrap(
         width = "15rem", # Minimum width for each input box before wrapping
-        selectizeInput(
-          inputId = NS(id, "provider"),
-          label = NULL,
-          choices = NULL,
-          multiple = TRUE,
-          options = list(maxOptions = 6000)
-        ),
         selectInput(
           inputId = NS(id, "year"),
           label = "Select academic year",
@@ -40,53 +29,68 @@ subjects_standards_ui <- function(id) {
           label = "Select measure",
           choices = c(sas_measure_choices)
         ),
-        selectInput(
-          inputId = NS(id, "level"),
-          label = "Select apprenticeship level",
-          choices = c(sas_level_choices),
-          multiple = TRUE,
-          selected = data_choices(data = sas_parquet, column = "apps_Level")
-        ),
       )
     ),
-    card(
-      layout_columns(
-        col_widths = c(4, 8),
-        # Provider list
-        card(
-          card_header(textOutput(NS(id, "sas_provider_table_title"))),
-          card_body(reactable::reactableOutput(NS(id, "sas_provider_table")))
+    # Main data area ===========================================================
+    layout_columns(
+      col_widths = c(4, 4, 4),
+      selectizeInput(
+        inputId = NS(id, "provider"),
+        label = NULL,
+        choices = NULL,
+        multiple = TRUE,
+        options = list(maxOptions = 6000)
+      ),
+      selectizeInput(
+        inputId = NS(id, "level"),
+        label = NULL,
+        choices = c(sas_measure_choices),
+        multiple = TRUE
+      ),
+      selectizeInput(
+        inputId = NS(id, "standard"),
+        label = NULL,
+        choices = NULL,
+        multiple = TRUE,
+        options = list(maxOptions = 6000)
+      ),
+    ),
+    # Provider list --------------------------------------------------------
+    layout_columns(
+      col_widths = c(4, 8),
+      card(
+        card_header(textOutput(NS(id, "sas_provider_table_title"))),
+        card_body(reactable::reactableOutput(NS(id, "sas_provider_table")))
+      ),
+      ## Tabs ----------------------------------------------------------------
+      navset_card_tab(
+        id = "sas_tabs",
+        # nav_panel(
+        #  "Graphic",
+        #  girafeOutput(NS(id, "subject_area_bar")),
+        #  ),
+        nav_panel(
+          "Table",
+          reactable::reactableOutput(NS(id, "sas_subject_area_table"))
         ),
-        ## Tabs ----------------------------------------------------------------
-        navset_card_tab(
-          id = "sas_tabs",
-          nav_panel(
-            "Graphic",
-            girafeOutput(NS(id, "subject_area_bar")),
-          ),
-          nav_panel(
-            "Table including level and standard",
-            reactable::reactableOutput(NS(id, "sas_subject_area_table"))
-          ),
-          nav_panel(
-            "Download data",
-            shinyGovstyle::radio_button_Input(
-              inputId = NS(id, "file_type"),
-              label = h2("Choose download file format"),
-              hint_label = "This will download all data related to the providers and options selected.
+        nav_panel(
+          "Download data",
+          shinyGovstyle::radio_button_Input(
+            inputId = NS(id, "file_type"),
+            label = h2("Choose download file format"),
+            hint_label = "This will download all data related to the providers and options selected.
           The XLSX format is designed for use in Microsoft Excel",
-              choices = c("CSV (Up to 13.18 MB)", "XLSX (Up to 2.12 MB)"),
-              selected = "CSV (Up to 13.18 MB)"
-            ),
-            # Bit of a hack to force the button not to be full width
-            layout_columns(
-              col_widths = 3,
-              downloadButton(
-                NS(id, "download_data"),
-                label = "Download data",
-                class = "gov-uk-button",
-                icon = NULL
-              )
+            choices = c("CSV (Up to 13.18 MB)", "XLSX (Up to 2.12 MB)"),
+            selected = "CSV (Up to 13.18 MB)"
+          ),
+          # Bit of a hack to force the button not to be full width
+          layout_columns(
+            col_widths = 3,
+            downloadButton(
+              NS(id, "download_data"),
+              label = "Download data",
+              class = "gov-uk-button",
+              icon = NULL
             )
           )
         )
@@ -106,7 +110,23 @@ subject_standards_server <- function(id) {
       choices = c(sas_provider_choices),
       server = TRUE
     )
+    updateSelectizeInput(
+      session = session,
+      inputId = "level",
+      label = "Select level",
+      choices = c(sas_level_choices),
+      selected = c(sas_level_choices),
+      server = TRUE
+    )
+    updateSelectizeInput(
+      session = session,
+      inputId = "standard",
+      label = "Search for standard",
+      choices = c(sas_standard_choices),
+      server = TRUE
+    )
 
+    # Select a subject area from the table
     subject_selection <- reactive({
       if (is.null(input$subject_area_bar_selected)) {
         "all subjects"
@@ -122,12 +142,23 @@ subject_standards_server <- function(id) {
         filter(
           measure == input$measure,
           year == input$year,
-          apps_Level %in% input$level
         )
       if (!(is.null(input$provider))) {
         data <- data %>%
           filter(
             provider_name %in% input$provider
+          )
+      }
+      if (!(is.null(input$level))) {
+        data <- data %>%
+          filter(
+            apps_Level %in% input$level
+          )
+      }
+      if (!(is.null(input$standard))) {
+        data <- data %>%
+          filter(
+            std_fwk_name %in% input$standard
           )
       }
       data
@@ -155,14 +186,15 @@ subject_standards_server <- function(id) {
       # put in message if no levels selected, as get error
       validate(need(!is.null(input$level) > 0, paste0("Select apprenticeship level(s) to continue.")))
 
-      # Filter the data based on whether the user's selected any subject areas
-      # from the chart
+      # Filter the provider data based on whether the user's selected any subject areas
+      # from the table
       if (!is.null(input$subject_area_bar_selected)) {
         provider_data <- subject_area_data() %>%
           filter(ssa_t1_desc %in% ssa_t1_selected())
       } else {
         provider_data <- subject_area_data()
       }
+
       # Run a quick aggregate of numbers by provider name.
       provider_data %>%
         summarise(
@@ -191,37 +223,37 @@ subject_standards_server <- function(id) {
 
     # Create an interactive chart showing the numbers broken down by subject
     # area
-    output$subject_area_bar <- renderGirafe(
-      girafe(
-        ggobj =
-          subject_area_data() %>%
-            # The bar chart won't appear when there are none of the measure,
-            # nil measures still appear in the table
-            filter(values > 0) %>%
-            summarise( # nolint: indentation_linter
-              values = sum(values),
-              .by = c("ssa_t1_desc")
-            ) %>%
-            mutate(ssa_t1_desc = str_wrap(ssa_t1_desc, 32)) %>%
-            ggplot(
-              aes(
-                x = reorder(ssa_t1_desc, values),
-                y = values,
-                tooltip = ssa_t1_desc,
-                data_id = ssa_t1_desc
-              )
-            ) +
-            geom_col_interactive(fill = "#12436D") +
-            theme_classic() +
-            coord_flip() +
-            xlab("") +
-            ylab(input$measure),
-        options = list(opts_selection(
-          type = "multiple",
-          css = "fill:#28A197;stroke:#28A197;r:5pt;"
-        ))
-      )
-    )
+    #  output$subject_area_bar <- renderGirafe(
+    #    girafe(
+    #    ggobj =
+    #     subject_area_data() %>%
+    # The bar chart won't appear when there are none of the measure,
+    #     # nil measures still appear in the table
+    #     filter(values > 0) %>%
+    #     summarise( # nolint: indentation_linter
+    #       values = sum(values),
+    ##       .by = c("ssa_t1_desc")
+    #     ) %>%
+    #     mutate(ssa_t1_desc = str_wrap(ssa_t1_desc, 32)) %>%
+    #     ggplot(
+    #      aes(
+    #        x = reorder(ssa_t1_desc, values),
+    #       y = values,
+    #        tooltip = ssa_t1_desc,
+    #        data_id = ssa_t1_desc
+    #       )
+    #     ) +
+    #    geom_col_interactive(fill = "#12436D") +
+    #     theme_classic() +
+    #     coord_flip() +
+    #     xlab("") +
+    #     ylab(input$measure),
+    # options = list(opts_selection(
+    #   type = "multiple",
+    #   css = "fill:#28A197;stroke:#28A197;r:5pt;"
+    # ))
+    # )
+    # )
 
     # Expandable table of subject areas.
     output$sas_subject_area_table <- renderReactable({
@@ -236,10 +268,10 @@ subject_standards_server <- function(id) {
           values = sum(values),
           .by = c("ssa_t1_desc", "ssa_t2_desc", "apps_Level", "std_fwk_name")
         )
-      if (!is.null(input$subject_area_bar_selected)) {
-        subject_data <- subject_data %>%
-          filter(ssa_t1_desc %in% ssa_t1_selected())
-      }
+      #  if (!is.null(input$subject_area_bar_selected)) {
+      #   subject_data <- subject_data %>%
+      #    filter(ssa_t1_desc %in% ssa_t1_selected())
+      #  }
       reactable(
         subject_data %>%
           rename(
@@ -284,7 +316,7 @@ subject_standards_server <- function(id) {
         } else {
           # Added a basic pop up notification as the Excel file can take time to generate
           pop_up <- showNotification("Generating download file", duration = NULL)
-          openxlsx::write.xlsx(chars_reactive_table(), file, colWidths = "Auto")
+          openxlsx::write.xlsx(subject_area_data(), file, colWidths = "Auto")
           on.exit(removeNotification(pop_up), add = TRUE)
         }
       }

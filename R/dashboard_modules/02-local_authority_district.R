d8e6e5ef833050dba233ba46eeb9ca9370810612
COOPER <Alison.Cooper@EDUCATION.GOV.UK>
2024-12-19 17:23:47 +0000

    styling

--- R/dashboard_modules/02-local_authority_district.R
+++ R/dashboard_modules/02-local_authority_district.R
@@ -305,9 +305,9 @@ lad_server <- function(id) {
       )
 
       # Choose the boundary based on the year selection from the user
-      # think this will sort the hard -coding for the boundary list 
+      # think this will sort the hard -coding for the boundary list
       # will match to the first bit of the string - just the academic year & not qr
-      return(boundary_list[[substring(input$year,1,7)]])
+      return(boundary_list[[substring(input$year, 1, 7)]])
     })
 
     delivery_map_data <- reactive({
